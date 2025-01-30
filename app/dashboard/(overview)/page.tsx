510241ac5d70b52a7c49ecb8d969a303e906839d
olwethu-sigwela <olwethusigwela22@gmail.com>
2025-01-30 14:18:29 +0200

    Fixed error in CardsSkeleton reference in dashboard's page.tsx file.

--- app/dashboard/(overview)/page.tsx
+++ app/dashboard/(overview)/page.tsx
@@ -22,7 +22,7 @@ export default async function Page(){
             </h1>
 
             <div className = "grid gap-6 sm:grid-cols-2 lg:grid-cols-4">
-             <Suspense fallback = {CardsSkeleton}>
+             <Suspense fallback = {<CardsSkeleton/>}>
                 <CardWrapper/>
              </Suspense>
             </div>
